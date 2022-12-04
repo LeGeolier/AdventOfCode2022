df4aef6f584d1c018cc1d6db1e31a13eb5b1cbf7
JROELENS <84103188+LeGeolier@users.noreply.github.com>
2022-12-04 18:18:31 +0100

    feat: :sparkles: Added the main file and ressource for Exercice1 + ReadFile

---
+++ 01/main.py
@@ -0,0 +1,7 @@
+# This program should be able to :
+#   1. Open a file and read it line by line
+#   2. Sum all the value who aren't broken by two consecutive "\n"
+#   3. Put the summed value into a list
+#   4. When the whole file have been read we close it
+#   5. Then we fetch the Maximum value of this list and it's index.
+
