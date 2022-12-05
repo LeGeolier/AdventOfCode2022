adff64a36aa5802e4cf69b2c3a14173fb966dc58
JROELENS <84103188+LeGeolier@users.noreply.github.com>
2022-12-05 09:36:48 +0100

    feat: :sparkles: Part One done + made a refacto of the files names since each exercice is made of two part

--- 01/mainPart1.py
+++ 01/mainPart1.py
@@ -5,3 +5,18 @@
 #   4. When the whole file have been read we close it
 #   5. Then we fetch the Maximum value of this list and it's index.
 
+def main():
+    f = open("./01/ressource.txt","r")
+    response = []
+    value = 0
+    for line in f:
+        if len(line) == 1:
+            response.append(value)
+            value = 0
+        else:
+            value += int(line)
+    print(max(response))
+    return
+
+
+main()
